6e24cd71695dc4ec7c0b224b3b73c0022f32370a
Yuanjian Liu <yuanjian@uchicago.edu>
2024-02-27 17:00:15 -0600

    globus compute is working!

--- globus_compute_util.py
+++ globus_compute_util.py
@@ -3,3 +3,17 @@ import globus_compute_sdk
 def list_dir(path):
     import os
     return os.listdir(path)
+
+def list_cpu():
+    import subprocess
+    command = "lscpu"
+    return subprocess.check_output(command, shell=True).decode().strip()
+
+def remove_files(files):
+    import os
+    try:
+        for file in files:
+            os.remove(file)
+    except Exception as e:
+        return False
+    return True
--- pyqt5app.py
+++ pyqt5app.py
@@ -1,6 +1,5 @@
 import sys
 from PyQt5 import QtWidgets, uic
-
 from PyQt5.QtWidgets import *
 from PyQt5.QtGui import *
 from PyQt5 import QtGui, QtCore
@@ -11,6 +10,13 @@ import globus_utils
 import globus_sdk
 
 import yaml
+
+from globus_compute_sdk import Client, Executor
+from globus_compute_sdk.serialize import CombinedCode
+
+from globus_compute_util import list_dir, list_cpu, remove_files
+
+from pathlib import Path
 
 class UI(QDialog):
     def __init__(self):
@@ -25,6 +31,8 @@ class UI(QDialog):
         self.globus_id_lineedit_a = self.findChild(QLineEdit, "globus_id_lineedit_a")
         self.workdir_lineedit_a = self.findChild(QLineEdit, "workdir_lineedit_a")
 
+        self.register_globus_compute_button_a = self.findChild(QPushButton, "register_globus_compute_button_a")
+        self.remove_files_button_a = self.findChild(QPushButton, "remove_files_button_a")
         self.list_workdir_button_a = self.findChild(QPushButton, "list_workdir_button_a")
         self.save_config_button_a = self.findChild(QPushButton, "save_config_button_a")
         self.load_config_button_a = self.findChild(QPushButton, "load_config_button_a")
@@ -33,13 +41,17 @@ class UI(QDialog):
         self.transfer_button_a = self.findChild(QPushButton, "transfer_button_a")
         self.auto_transfer_button_a = self.findChild(QPushButton, "auto_transfer_button_a")
 
-        self.workdir_listwidget_a = self.findChild(QListWidget, "workdir_list_widget_b")
+        self.workdir_listwidget_a = self.findChild(QListWidget, "workdir_listwidget_a")
+        self.workdir_listwidget_a.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        self.workdir_listwidget_a.itemSelectionChanged.connect(self.on_listwidget_item_changed_a)
 
         # Machine B Config
         self.funcx_id_lineedit_b = self.findChild(QLineEdit, "funcx_id_lineedit_b")
         self.globus_id_lineedit_b = self.findChild(QLineEdit, "globus_id_lineedit_b")
         self.workdir_lineedit_b = self.findChild(QLineEdit, "workdir_lineedit_b")
 
+        self.register_globus_compute_button_b = self.findChild(QPushButton, "register_globus_compute_button_b")
+        self.remove_files_button_b = self.findChild(QPushButton, "remove_files_button_b")
         self.list_workdir_button_b = self.findChild(QPushButton, "list_workdir_button_b")
         self.save_config_button_b = self.findChild(QPushButton, "save_config_button_b")
         self.load_config_button_b = self.findChild(QPushButton, "load_config_button_b")
@@ -49,6 +61,8 @@ class UI(QDialog):
         self.auto_transfer_button_b = self.findChild(QPushButton, "auto_transfer_button_b")
 
         self.workdir_listwidget_b = self.findChild(QListWidget, "workdir_listwidget_b")
+        self.workdir_listwidget_b.setSelectionMode(QtWidgets.QAbstractItemView.ExtendedSelection)
+        self.workdir_listwidget_b.itemSelectionChanged.connect(self.on_listwidget_item_changed_b)
 
         # Globus Transfer Authentication
         self.authenticate_button = self.findChild(QPushButton, "authenticate_button")
@@ -72,11 +86,38 @@ class UI(QDialog):
         self.transfer_button_b.clicked.connect(self.on_click_transfer_button_b)
         self.auto_transfer_button_b.clicked.connect(self.on_click_autotransfer_button_b)
         
+        self.register_globus_compute_button_a.clicked.connect(self.on_click_register_globus_compute_a)
+        self.register_globus_compute_button_b.clicked.connect(self.on_click_register_globus_compute_b)
+        self.remove_files_button_a.clicked.connect(self.on_click_remove_files_button_a)
+        self.remove_files_button_b.clicked.connect(self.on_click_remove_files_button_b)
+
         # YAML config information
         self.machine_a_config = None
         self.machine_b_config = None
 
+        # Globus Compute Client
+        self.gcc = Client(code_serialization_strategy=CombinedCode())
+        # self.list_dir_uuid = self.gcc.register_function(list_dir)
+        self.gce_machine_a = None
+        self.gce_machine_b = None
+
+        # ListWidget selection
+        self.listwidget_a_selected_paths = None
+        self.listwidget_b_selected_paths = None
+
         self.show()
+
+    def on_click_register_globus_compute_a(self):
+        self.gce_machine_a = Executor(endpoint_id=self.funcx_id_lineedit_a.text().strip(), client=self.gcc)
+        future = self.gce_machine_a.submit(list_cpu)
+        print("submitted a lscpu to machine A")
+        future.add_done_callback(lambda f: print("Machine A CPU Info:\n", f.result()))
+
+    def on_click_register_globus_compute_b(self):
+        self.gce_machine_b = Executor(endpoint_id=self.funcx_id_lineedit_b.text().strip(), client=self.gcc)
+        future = self.gce_machine_b.submit(list_cpu)
+        print("submitted a lscpu to machine B")
+        future.add_done_callback(lambda f: print("Machine B CPU Info:\n", f.result()))
 
     def on_click_authenticate_button(self):
         collections = [self.globus_id_lineedit_a.text().strip(), self.globus_id_lineedit_b.text().strip()]
@@ -109,14 +150,86 @@ class UI(QDialog):
         color_effect = QGraphicsColorizeEffect()
         color_effect.setColor(Qt.GlobalColor.darkGreen) 
         self.authenticate_status_label.setGraphicsEffect(color_effect)
-        
-        QMessageBox.information(self, "Authenticate", "You have finished Globus Autentication", QMessageBox.StandardButton.Close)
+
+        QMessageBox.information(self, "Authenticate", "You have finished Globus Transfer Autentication", QMessageBox.StandardButton.Close)
+
+    def remove_files_callback(self, future, machine):
+        success = future.result()
+        if success:
+            print(f"the files have been removed! on machine {machine}")
+        else:
+            print("the file removal was not successful!")
+        if machine == 'A':
+            self.on_click_list_workdir_button_a()
+        elif machine == 'B':
+            self.on_click_list_workdir_button_b()
+
+
+    def on_click_remove_files_button_a(self):
+        reply = QMessageBox.question(self, "Remove Files", "Are you sure you want to remove selected files in Machine A?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+        if reply == QMessageBox.StandardButton.Yes:
+            print("Confirmed to remove files in Machine A")
+        else:
+            print("Removing files in machine A is canceled")
+            return
+        if len(self.listwidget_a_selected_paths) > 0:
+            future = self.gce_machine_a.submit(remove_files, self.listwidget_a_selected_paths)
+            future.add_done_callback(lambda f: self.remove_files_callback(f, "A"))
+        else:
+            print("You haven't selected files to remove!")
+
+
+    def on_click_remove_files_button_b(self):
+        reply = QMessageBox.question(self, "Remove Files", "Are you sure you want to remove selected files in Machine B?", QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+        if reply == QMessageBox.StandardButton.Yes:
+            print("Confirmed to remove files in Machine B")
+        else:
+            print("Removing files in machine B is canceled")
+            return
+        if len(self.listwidget_b_selected_paths) > 0:
+            future = self.gce_machine_b.submit(remove_files, self.listwidget_b_selected_paths)
+            future.add_done_callback(lambda f: self.remove_files_callback(f, "B"))
+        else:
+            print("You haven't selected files to remove!")
+
+
+    def on_listwidget_item_changed_a(self):
+        self.listwidget_a_selected_paths = [Path(self.workdir_lineedit_a.text().strip()) / item.text().strip()
+                                           for item in self.workdir_listwidget_a.selectedItems()]
+        print("list widget a selected:", self.listwidget_a_selected_paths)
+
+    def on_listwidget_item_changed_b(self):
+        self.listwidget_b_selected_paths = [Path(self.workdir_lineedit_b.text().strip()) / item.text().strip()
+                                           for item in self.workdir_listwidget_b.selectedItems()]
+        print("list widget b selected:", self.listwidget_b_selected_paths)
+
+    def put_workdir_into_listWidget_a(self, future):
+        self.workdir_listwidget_a.clear()
+        for file in future.result():
+            self.workdir_listwidget_a.addItem(file)
+
+    def put_workdir_into_listWidget_b(self, future):
+        self.workdir_listwidget_b.clear()
+        for file in future.result():
+            self.workdir_listwidget_b.addItem(file)
 
     def on_click_list_workdir_button_a(self):
-        QMessageBox.information(self, "List Workdir", "You clicked the List Workdir A button!", QMessageBox.StandardButton.Close)
+        if self.gce_machine_a is None:
+            QMessageBox.information(self, "List Workdir", "You need to register Globus Compute First!", QMessageBox.StandardButton.Close)
+            return
+        future = self.gce_machine_a.submit(list_dir, self.workdir_lineedit_a.text().strip())
+        future.add_done_callback(lambda f: self.put_workdir_into_listWidget_a(f))
+        print("submitted request to list workdir for machine A")
+        # QMessageBox.information(self, "List Workdir", "You clicked the List Workdir A button!", QMessageBox.StandardButton.Close)
 
     def on_click_list_workdir_button_b(self):
-        QMessageBox.information(self, "List Workdir", "You clicked the List Workdir B button!", QMessageBox.StandardButton.Close)
+        if self.gce_machine_b is None:
+            QMessageBox.information(self, "List Workdir", "You need to register Globus Compute First!", QMessageBox.StandardButton.Close)
+            return
+        future = self.gce_machine_b.submit(list_dir, self.workdir_lineedit_b.text().strip())
+        future.add_done_callback(lambda f: self.put_workdir_into_listWidget_b(f))
+        print("submitted request to list workdir for machine B")
+        # QMessageBox.information(self, "List Workdir", "You clicked the List Workdir B button!", QMessageBox.StandardButton.Close)
 
     def on_click_save_config_button_a(self):
         QMessageBox.information(self, "Save Config", "You clicked the Save Config A button!", QMessageBox.StandardButton.Close)
@@ -126,6 +239,9 @@ class UI(QDialog):
 
     def on_click_load_config_button_a(self):
         machine_config_file, ok = QFileDialog.getOpenFileName(self, "Open", os.getcwd(), "YAML files (*.yaml *.yml)")
+        if not ok:
+            return
+        
         with open(machine_config_file, 'r') as f:
             try:
                 self.machine_a_config = yaml.safe_load(f)
@@ -144,6 +260,8 @@ class UI(QDialog):
 
     def on_click_load_config_button_b(self):
         machine_config_file, ok = QFileDialog.getOpenFileName(self, "Open", os.getcwd(), "YAML files (*.yaml *.yml)")
+        if not ok:
+            return
         with open(machine_config_file, 'r') as f:
             try:
                 self.machine_b_config = yaml.safe_load(f)
